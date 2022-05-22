8a65609ee42c164b542b83225e5a12d46c086d30
nnimal <nnimalan@gmail.com>
2022-05-21 23:27:44 -0700

    add comments

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+interface Year_01{
+    public function book_order();
+    public function name_list();
+    public function marks_list();
+}
+
+class Main_school implements Year_01{
+ public string $stu_name;
+ public string $std_city;
+ private int $stu_age;
+ public string $stu_studing_year;
+
+ public function __construct(){
+
+ }
+public function setAge($age){
+    $this->stu_age = $age;
+}
+
+public function getAge(){
+    return $this->stu_age;
+}
+
+
+ public function book_order(){
+
+ }
+
+ public function name_list(){
+
+ }
+ public function marks_list(){
+
+
+ }
+
+}
+
+//comments made
+// new line
+
+?>
+
